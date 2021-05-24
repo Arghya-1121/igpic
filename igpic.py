bf8af02d7217f114eaef3110903bd1c4e7295730
Arghya Sarkar <63844005+Arghya-1121@users.noreply.github.com>
2021-05-24 10:46:13 +0530

    Update igpic.py

--- igpic.py
+++ igpic.py
@@ -1,6 +1,12 @@
 import instaloader
 
-ig = instaloader.Instaloader()
-profile = input("Enter instagram username:")
-ig.download_profile(profile, profile_pic_only=True)
+print (' _             _')
+print ('(_) __ _ ____ (_) ___')
+print ('| |/ _` |  _ \| |/ __|')
+print ('| | (_| | |_) | | (__')
+print ('|_|\__, | .__/|_|\___|')
+print ('   |___/|_|')
 
+ig = instaloader.Instaloader()
+profile = input("Enter Instagram username:")
+ig.download_profile(profile, profile_pic_only=True)
